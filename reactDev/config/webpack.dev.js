8d27dcf826389f7fa9d888bad726ce77facd0a30
Christian John Gonzalez <bournecreative@gmail.com>
2020-02-17 11:35:00 -0800

    reconfigure build for vanilla js for React.

---
+++ reactDev/config/webpack.dev.js
@@ -0,0 +1,39 @@
+const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: {
+        main: ["./src/index.js"]
+    },
+    mode: "development",
+    output: {
+        filename: "[name]-bundle.js",
+        path: path.resolve(__dirname, "../dist"),
+        publicPath: '/'
+    },
+    devServer: {
+        contentBase: "dist",
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/i,
+                use: ["babel-loader", "eslint-loader"],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })]
+}
---
+++ reactDev/src/Components/App.js
@@ -0,0 +1,19 @@
+import "../scss/styles.scss"
+import React from 'react';
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div className="hero">
+                        <h1>hello React Webpack Configuration</h1>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
